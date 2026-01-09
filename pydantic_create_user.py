cee7339ef40fb9ed7a8e8d765a8324f10342298f
nursltan <nursultanmuratjanov1@gmail.com>
2026-01-09 14:16:15 +0600

    Практика написания Pydantic-модели для create user

---
+++ pydantic_create_user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    id: str
+    email: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
